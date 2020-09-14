ca71f2684f3eee2bed72d1267a7c09caf6209010
monir-007 <monir.hossain@northsouth.edu>
2020-09-14 23:48:38 +0600

    change 4

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
-//import axios from 'axios';
+//import axios from "axios";
 
 const rootUrl = "https://api.github.com";
 
@@ -11,8 +11,13 @@ const GithubContext = React.createContext();
 //Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
