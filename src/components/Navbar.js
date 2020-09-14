2ab71f09d97ef182959441efdd082ebd21714f18
monir-007 <monir.hossain@northsouth.edu>
2020-09-14 21:13:58 +0600

    change part 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useAuth0 } from '@auth0/auth0-react';
+//import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
   return <h2>navbar component</h2>;
