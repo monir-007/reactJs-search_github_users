92784bbfe857781bf5e12257a3042b8526694b47
monir-007 <monir.hossain@northsouth.edu>
2020-09-16 00:26:37 +0600

    change no - 7

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import { MdSearch } from "react-icons/md";
 import { GithubContext } from "../context/context";
+
 const Search = () => {
   const [user, setUser] = React.useState("");
+  const { requests, error } = React.useContext(GithubContext);
+
   //get things from global context
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(user){
-      
+    if (user) {
     }
   };
 
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
@@ -24,10 +31,10 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">search</button>
+            {requests > 0 && <button type="submit">search</button>}
           </div>
         </form>
-        <h3>requests: 60/60</h3>
+        <h3>requests: {requests}/60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
-//import axios from "axios";
+import axios from "axios";
 
 const rootUrl = "https://api.github.com";
 
@@ -14,8 +14,41 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+
+  //request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  //error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "you have exceeded hourly limit! ");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  //setting up error
+  function toggleError(show, msg) {
+    setError({ show, msg });
+  }
+
+  //error
+
+  useEffect(checkRequests(), []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
