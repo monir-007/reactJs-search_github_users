c40189efabbb6d5096238e583d153dbe122789eb
monir-007 <monir.hossain@northsouth.edu>
2020-09-15 19:56:51 +0600

    change 5

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
+  const chartData = [
+    {
+      label: "Venezuela",
+      value: "290",
+    },
+    {
+      label: "Saudi",
+      value: "260",
+    },
+    {
+      label: "Canada",
+      value: "180",
+    },
+    {
+      label: "Iran",
+      value: "140",
+    },
+    {
+      label: "Russia",
+      value: "115",
+    },
+    {
+      label: "UAE",
+      value: "100",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData}/>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
 const Dashboard = () => {
   return (
     <main>
-      <Navbar></Navbar>
-      <Search/>
-      <Info/>
-      <User/>
-      <Repos/>
+      {/* <Navbar></Navbar> */}
+      {/* <Search/> */}
+      <Info />
+      <User />
+      <Repos />
     </main>
   );
 };
