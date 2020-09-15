905e5e0429e4d11f37606ac2e2715a6d0360ab3c
monir-007 <monir.hossain@northsouth.edu>
2020-09-16 00:34:48 +0600

    major change - 8 (got error)

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,6 @@ import { GithubContext } from "../context/context";
 
 const Search = () => {
   const [user, setUser] = React.useState("");
-  const { requests, error } = React.useContext(GithubContext);
 
   //get things from global context
   const handleSubmit = (e) => {
@@ -17,11 +16,6 @@ const Search = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
-        {error.show && (
-          <ErrorWrapper>
-            <p>{error.msg}</p>
-          </ErrorWrapper>
-        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
@@ -31,10 +25,10 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            {requests > 0 && <button type="submit">search</button>}
+            <button type="submit">search</button>
           </div>
         </form>
-        <h3>requests: {requests}/60</h3>
+        <h3>requests: 60/60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -15,40 +15,8 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
-  //request loading
-  const [requests, setRequests] = useState(0);
-  const [loading, setLoading] = useState(false);
-  //error
-  const [error, setError] = useState({ show: false, msg: "" });
-
-  // check rate
-  const checkRequests = () => {
-    axios(`${rootUrl}/rate_limit`)
-      .then(({ data }) => {
-        let {
-          rate: { remaining },
-        } = data;
-        setRequests(remaining);
-        if (remaining === 0) {
-          toggleError(true, "you have exceeded hourly limit! ");
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-
-  //setting up error
-  function toggleError(show, msg) {
-    setError({ show, msg });
-  }
-
-  //error
-
-  useEffect(checkRequests(), []);
-
   return (
-    <GithubContext.Provider
-      value={{ githubUser, repos, followers, requests, error }}
-    >
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
       {children}
     </GithubContext.Provider>
   );
